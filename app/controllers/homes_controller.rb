5e8ec101537e1a58a833efd6cd5796589f02d180
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-08-01 02:26:38 +0000

    8/1

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,12 @@
+class HomesController < ApplicationController
+  
+  def top
+  end  
+  
+  def entrance
+  end
+  
+  def about
+  end  
+  
+end
---
+++ app/javascript/packs/game.js
@@ -0,0 +1,14 @@
+var c = document.createElement("canvas");
+var ctx = c.getContent("2d");
+c.width = 500;
+c.height = 350;
+
+document.body.appendChild(c);
+
+function loop(){
+  
+  ctx.fillRect(0,0,c.width, c.height);
+  requestAnimationFrame(loop);
+}
+
+loop();
