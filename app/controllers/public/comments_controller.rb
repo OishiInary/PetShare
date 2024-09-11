61e584834b26be19ebaed84d374c401a20b64ed8
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-09-11 13:21:14 +0000

    コメント欄部分テンプレート

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,7 +5,8 @@ before_action :ensure_guest_user, only: [:create]
   def create
     @album = Album.find(params[:album_id])
     @comment = @album.comments.create(comment_params)
-    redirect_to album_path(@album)
+    @comment.save
+    # redirect_to album_path(@album)
   end
 
   private
