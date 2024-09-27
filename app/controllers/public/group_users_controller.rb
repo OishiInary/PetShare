6871576cf13c26b04f1bec889ba0d9757d74274b
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-09-27 01:05:25 +0000

    グループ脱退機能修正

--- app/controllers/public/group_users_controller.rb
+++ app/controllers/public/group_users_controller.rb
@@ -14,7 +14,7 @@ before_action :ensure_guest_user, only: [:create,:destroy]
   end
 
   def destroy
-      group_user = current_user.group_user.find_by(group_id: params[:group_id])
+      group_user = current_user.group_users.find_by(group_id: params[:group_id])
       group_user.destroy
     redirect_to request.referer
   end
