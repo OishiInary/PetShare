5880e389f43278d638c9909de0d5434bbbcd800c
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-09-27 02:19:39 +0000

    チャットを写真だけでも送信可能に

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -8,9 +8,9 @@ class Public::SearchesController < ApplicationController
     page_number = params[:page].present? ? params[:page] : 1
     # 選択したモデルに応じて検索を実行
     if @model  == "user"
-      @records = User.search_for(@content, @method)
+      @records = User.search_for(@content, @method).page(page_number).per(10).order(created_at: :desc)
     elsif @model  == "album"
-      @records = Album.search_for(@content, @method)
+      @records = Album.search_for(@content, @method).page(page_number).per(10).order(created_at: :desc)
     elsif @model == "pet"
       @records = Pet.search_for(@content, @method).page(page_number).per(10).order(created_at: :desc)
     else 
