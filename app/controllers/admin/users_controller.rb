51e58a28d1a1f6297acebde48bb0f0799552c1ff
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-09-27 09:20:26 +0000

    adminのuserをedit機能修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,17 +21,24 @@ before_action :authenticate_admin!
         @user = User.find(params[:id])
     end
 
-    def update
-      @user = User.find(params[:id])
-
-      if @user.update(user_params)
-        flash[:notice] = user_params[:password].present? ? "パスワードが更新されました" : "更新に成功しました"
-        redirect_to edit_admin_user_path(@user)
-      else
-        flash[:alert] = "更新に失敗しました"
-        redirect_back(fallback_location: root_path)
-      end
+  def update
+    @user = User.find(params[:id])
+  
+    # パスワードが空の場合はパスワード関連のフィールドを削除
+    if user_params[:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
     end
+    
+    if @user.update(user_params)
+      flash[:notice] = user_params[:password].present? ? "パスワードが更新されました" : "更新に成功しました"
+      redirect_to admin_user_path(@user)
+    else
+      flash[:alert] = "更新に失敗しました"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 
    
 
